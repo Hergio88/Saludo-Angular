6f0aa2b6ae72e9e3c378af9075718d354ede864b
SergioG <sl_garcia@yahoo.com.ar>
2024-07-03 14:51:25 -0300

    Se realiza la practica N6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SaludoComponent } from './saludo/saludo.component';
+import { UppercasePipe } from './uppercase.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SaludoComponent
+    SaludoComponent,
+    UppercasePipe
   ],
   imports: [
     BrowserModule,
